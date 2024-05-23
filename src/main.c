bade7638a13c77986d8c5ec57e5d73360bd7f9a6
Ana <acgs@cesar.school>
2024-05-23 08:55:03 -0300

    creating var.h with defines

--- src/main.c
+++ src/main.c
@@ -16,83 +16,7 @@
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
-
-// 1
-#define STARTIROOM1 8
-#define FINISHIROOM1 27
-#define STARTJROOM1 8
-#define FINISHJROOM1 16
-
-#define DOORI1 26
-#define DOORJ1 12
-
-#define STARTIHALL1 27
-#define FINISHIHALL1 32
-#define STARTJHALL1 11
-#define FINISHJHALL1 14
-
-// 2
-#define STARTIROOM2 34
-#define FINISHIROOM2 44
-#define STARTJROOM2 8
-#define FINISHJROOM2 20
-
-#define ENTERDOORI2 34
-#define ENTERDOORJ2 12
-
-#define EXITDOORI2 38
-#define EXITDOORJ2 19
-
-#define STARTIHALL2 36
-#define FINISHIHALL2 41
-#define STARTJHALL2 20
-#define FINISHJHALL2 24
-
-// 3
-#define STARTIROOM3 27
-#define FINISHIROOM3 85
-#define STARTJROOM3 24
-#define FINISHJROOM3 32
-
-#define ENTERDOORI3 38
-#define ENTERDOORJ3 24
-
-#define EXITDOORI3 84
-#define EXITDOORJ3 28
-
-#define STARTIHALL3 85
-#define FINISHIHALL3 92
-#define STARTJHALL3 27
-#define FINISHJHALL3 30
-
-// 4
-#define STARTIROOM4 94
-#define FINISHIROOM4 112
-#define STARTJROOM4 10
-#define FINISHJROOM4 32
-
-#define ENTERDOORI4 94
-#define ENTERDOORJ4 28
-
-#define EXITDOORI4 94
-#define EXITDOORJ4 13
-
-#define STARTIHALL4 92
-#define FINISHIHALL4 80
-#define STARTJHALL4 12
-#define FINISHJHALL4 15
-
-// 5 (boos entrance)
-#define STARTIROOM5 50
-#define FINISHIROOM5 80
-#define STARTJROOM5 7
-#define FINISHJROOM5 20
-
-#define ENTERDOORI5 94
-#define ENTERDOORJ5 13
-
-#define EXITDOORI5 94
-#define EXITDOORJ5 13
+#include "var.h"
 
 char *enemies[] = {"💀", "👿", "👹", "👻", "👽", "🧟", "🧛"};
 
@@ -762,8 +686,6 @@ int main()
         printf("fechou :D\n");
         return 0;
     }
-
-        
 
     else
     {
